8d02d342a1ce7532d3bed6042e6b45e401e83999
davidruddell <69439720+davidruddell@users.noreply.github.com>
2022-11-15 14:36:02 -0700

    init upload

---
+++ SFWE301Project/src/Main.java
@@ -0,0 +1,43 @@
+ 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.StageStyle;
+
+
+public class Main extends Application {
+ 
+ @Override
+ public void start(Stage stage) {
+  try {
+
+   stage.initStyle(StageStyle.UNDECORATED);
+   Parent root = FXMLLoader.load(getClass().getResource("fxml/Homepage.fxml"));
+   Scene scene = new Scene(root);
+   stage.setScene(scene);
+
+   Screen screen = Screen.getPrimary();
+   Rectangle2D bounds = screen.getVisualBounds();
+
+   stage.setX(bounds.getMinX());
+   stage.setY(bounds.getMinY());
+   stage.setWidth(bounds.getWidth());
+   stage.setHeight(bounds.getHeight());
+
+   
+   stage.show();
+   
+  } catch(Exception e) {
+   e.printStackTrace();
+  }
+ } 
+
+ public static void main(String[] args) {
+  launch(args);
+  
+ }
+}
